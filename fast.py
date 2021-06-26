99582e514dc76138884eedf9a30eecb098741895
Ethan He <yfhe@ucdavis.edu>
2021-06-26 10:13:11 +0800

    remove url_util

--- fast.py
+++ fast.py
@@ -1,12 +1,28 @@
 import sys
 import os
-from url_util import change_url
+from func_util import foldl
 
 if len(sys.argv) != 2:
-    sys.stderr("Usage: fast github_url\n")
+    print("Usage: fast github_url")
     exit(1)
 
 url = sys.argv[1]
-new_url = change_url(url)
+spurl = url.split('/')
+    
+def merge_url(acc, s) -> str:
+    if acc == "":
+        return s
+    
+    if s == "github.com":
+        suffix = s + ".cnpmjs.org"
+    elif s == "\n":
+        suffix = "/"
+    else:
+        suffix = s
+    
+    return acc + "/" + suffix
+
+new_url = foldl(merge_url, "", spurl)
+
 print("git clone " + new_url)
 os.execv("/usr/bin/git", ("git", "clone", new_url))
